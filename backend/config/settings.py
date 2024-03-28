bfa4b70b9d6bccf1c4d88a4baa4c8263c0c0fc74
Kostek <ksytnik73kwer@gmail.com>
2024-03-28 19:34:32 +0100

    Update database configuration in settings.py and remove duplicate admin URL in urls.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from .config import load_config
+
+db = load_config().db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -76,9 +78,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": db.database,
+        "USER": db.user,
+        "PASSWORD": db.password,
+        "HOST": db.host,
+        "PORT": db.port,
     }
 }
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,7 +21,6 @@ from api import urls as rest_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(rest_urls))
 ]
