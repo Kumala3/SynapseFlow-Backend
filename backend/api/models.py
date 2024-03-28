cc11ffbd7e0f0dbfe2f459c6dc8a87a8d1b7c987
Kostek <ksytnik73kwer@gmail.com>
2024-03-28 19:34:16 +0100

    Add FaqAnswer model, migration script for this model Add config file to get access to my virtual environment

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class FaqAnswer(models.Model):
+    answer_id = models.BigAutoField(verbose_name="FAQ ID", primary_key=True)
+    title = models.CharField(max_length=255, verbose_name="Title")
+    answer = models.TextField(max_length=1000, verbose_name="Answer")
+    created_at = models.DateTimeField(verbose_name="Created at", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Updated at", auto_now=True)
