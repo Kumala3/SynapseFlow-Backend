6bdbe76ba96e7e883a190bb1200792d642ad0733
Kostek <ksytnik73kwer@gmail.com>
2024-04-01 16:30:16 +0200

    Add PartnerCompaniesView to API URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FaqQuestion, FaqAnswersList
+from .views import FaqQuestion, FaqAnswersList, PartnerCompaniesView
 
 urlpatterns = [
     path("faq-question/", FaqQuestion.as_view()),
     path("faq-answers/", FaqAnswersList.as_view()),
+    path("partner-companies/", PartnerCompaniesView.as_view()),
 ]
