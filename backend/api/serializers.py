676d2dced883cd2a01be9080d30c6eb081b57c3c
Kostek Sytnyk <ksytnik73kwer@gmail.com>
2024-04-12 16:37:10 +0200

    Create pricing-plans Endpoint (#3) (#5)
    * Add Plan and PlanAdvantage models
    
    * Add migrations for Plan and PlanAdvantage models
    
    * Refactor buttonText parameters in predefined_plans to button_text in migration
    Registered Plan and PlanAdvantage to Django-Admin-Panel
    
    * Add migrations for inserting plans advantages
    
    * Update docker-compose.yml and nginx configuration
    
    * Fix issues with migrations
    
    * Add FaqQuestion model and serializer for this model
    
    * Refactor plan-related models names and refactor migrations upon them
    
    * Create PricingPlanAdvantageSerializer and PricingPlanSerializer serializers
    Create new PricingPlansView to retrieve pricing-plans
    
    * Refactor models name in admin.py
    Add "pricing-plans/" to urls
    Update api url to v1 like a first version of my API

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
-from .models import FaqAnswer, PartnerCompany, FaqQuestion
+from .models import (
+    FaqAnswer,
+    PartnerCompany,
+    FaqQuestion,
+    PricingPlan,
+    PricingPlanAdvantage,
+)
 
 
 class FaqAnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = FaqAnswer
         fields = ["title", "content"]
-
-
-class PartnerCompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PartnerCompany
-        fields = ["company_name", "company_logo", "company_website"]
 
 
 class FaqQuestionSerializer(serializers.ModelSerializer):
@@ -21,3 +21,23 @@ class FaqQuestionSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return FaqQuestion.objects.create(**validated_data)
+
+
+class PartnerCompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PartnerCompany
+        fields = ["company_name", "company_logo", "company_website"]
+
+
+class PricingPlanAdvantageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PricingPlanAdvantage
+        fields = ["advantage"]
+
+
+class PricingPlanSerializer(serializers.ModelSerializer):
+    advantages = PricingPlanAdvantageSerializer(many=True)
+
+    class Meta:
+        model = PricingPlan
+        fields = ["plan", "description", "cost", "button_text", "advantages"]
--- backend/django-config/urls.py
+++ backend/django-config/urls.py
@@ -22,5 +22,5 @@ from api import urls as rest_urls
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include(rest_urls))
+    path("v1/", include(rest_urls))
 ]
