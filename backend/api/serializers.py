1f93910c22540560918669bd80280c3657f43856
Kostek <ksytnik73kwer@gmail.com>
2024-04-14 23:04:43 +0200

    Update email validation and question length validation in FaqQuestionSerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,6 +7,8 @@ from .models import (
     PricingPlanAdvantage,
 )
 
+from email_validator import validate_email as validate_email_address, EmailNotValidError
+
 
 class FaqAnswerSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,9 +17,27 @@ class FaqAnswerSerializer(serializers.ModelSerializer):
 
 
 class FaqQuestionSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    question = serializers.CharField(max_length=800)
+
     class Meta:
         model = FaqQuestion
         fields = ["question", "email"]
+
+    def validate_email(self, value):
+        try:
+            valid = validate_email_address(value)
+            email = valid.email
+        except EmailNotValidError as e:
+            raise serializers.ValidationError(str(e))
+        return email
+
+    def validate_question(self, question):
+        if len(question) < 50 or len(question) > 800:
+            raise serializers.ValidationError(
+                "Question must be at least 50 characters and no more than 800 characters"
+            )
+        return question
 
     def create(self, validated_data):
         return FaqQuestion.objects.create(**validated_data)
