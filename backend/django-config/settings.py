ecd50fcc3a75f4bed23340db0adf154c9cdd2301
Kostek <ksytnik73kwer@gmail.com>
2024-05-26 21:08:11 +0200

    Update DEBUG setting, and STATIC_ROOT parameters

--- backend/django-config/settings.py
+++ backend/django-config/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = misc.secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["api.candeel.me", "localhost"]
 
@@ -153,7 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
